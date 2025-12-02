d7564e1074cf49aad7a8a018d7d901d08ebe7ffc
Augustus Chong <augustus.chong@gmail.com>
2025-12-03 00:32:22 +0800

    added mlp

--- mnist.py
+++ mnist.py
@@ -8,14 +8,15 @@ import os
 from PIL import Image 
 import torch.nn.functional as F
 
-BATCH_SIZE = 16
-LEARNING_RATE = 0.0001
-EPOCHS = 40
+BATCH_SIZE = 64
+LEARNING_RATE = 0.001
+EPOCHS = 30
 DOWNLOAD_ROOT = './mnist_data'
 INPUT_SIZE = 28 * 28
 NUM_CLASSES = 10
 CUSTOM_IMAGE_PATH = 'custom_digit.png' 
-MODEL_SAVE_PATH = 'logistic_model.pth'
+MODEL_SAVE_PATH = 'mlp_model.pth'
+HIDDEN_SIZE = 128
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
@@ -55,6 +56,24 @@ class LogisticRegression(nn.Module):
     def forward(self, x):
         x = x.view(x.size(0), -1) #flatten image into vector
         out = self.linear(x)
+        return out
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super().__init__()
+        
+        # 1. Input Layer to Hidden Layer
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        # 2. Hidden Layer to Output Layer
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+        
+    def forward(self, x):
+        x = x.view(x.size(0), -1) 
+        # First Linear transformation + Activation Function (ReLU)
+        x = F.relu(self.fc1(x))
+        
+        # Second Linear transformation to output scores
+        out = self.fc2(x)
         return out
     
 def train_model(model, train_loader, criterion, optimizer, epochs, device):
@@ -143,7 +162,8 @@ if __name__ == '__main__':
     #Get dataloaders
     train_loader, test_loader = get_data_loaders(DOWNLOAD_ROOT, BATCH_SIZE)
     #Initialize model
-    model = LogisticRegression(INPUT_SIZE, NUM_CLASSES).to(device)
+    model = MLP(INPUT_SIZE, HIDDEN_SIZE, NUM_CLASSES).to(device)
+    #model = LogisticRegression(INPUT_SIZE, NUM_CLASSES).to(device)
 
     #load saved weights
     is_loaded = load_model(model, MODEL_SAVE_PATH, device)
